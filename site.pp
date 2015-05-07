9901e30c52f691b4f7926165fb3e80bc42011006
quikslvr <yu.elistratov@gmail.com>
2015-05-07 18:17:32 +0300

    Create site.pp

---
+++ site.pp
@@ -0,0 +1,23 @@
+class passwd {
+    file { "/etc/passwd":
+	owner => root,
+	group => root,
+	mode => 644,
+    }
+}
+
+class nginx {
+    package { "nginx":
+	ensure => latest,
+    }
+    service { "nginx":
+	ensure => running,
+	require => Package["nginx"],
+    }
+}
+
+
+node default {
+    include passwd
+    include nginx
+}
