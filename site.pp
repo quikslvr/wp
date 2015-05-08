17f65e6fe97e5e00717f208c0354545d8370c1f1
quikslvr <yu.elistratov@gmail.com>
2015-05-08 18:15:36 +0300

    Update site.pp

--- site.pp
+++ site.pp
@@ -18,11 +18,11 @@ class nginx {
 	source => "puppet://puppet.server/files/cfg/wp.example.net",
 	mode => 644,
 	require => Package["nginx"],
-	exec { "reload_nginx":
+    }
+    exec { "reload_nginx":
     		command => "/etc/init.d/nginx reload",
     		#path    => "/usr/local/bin/:/bin/",
     		# path    => [ "/usr/local/bin/", "/bin/" ],  # alternative syntax
-	}
     }
 }
 
@@ -45,12 +45,12 @@ class php-fpm {
 	source => "puppet://puppet.server/files/cfg/php-fpm.conf",
 	mode => 644,
 	require => Package["php5-fpm"],
-	exec { "reload_php-fpm":
+    }
+    exec { "reload_php-fpm":
     		command => "/etc/init.d/php-fpm reload",
     		#path    => "/usr/local/bin/:/bin/",
     		# path    => [ "/usr/local/bin/", "/bin/" ],  # alternative syntax
 	}
-    }
 }
 
 class mysql {
