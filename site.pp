20e4042291a12af4ea14ceeaccc460638e5c6283
quikslvr <yu.elistratov@gmail.com>
2015-05-08 18:53:25 +0300

    Update site.pp

--- site.pp
+++ site.pp
@@ -25,7 +25,10 @@ class nginx {
     }
     exec { "make_symlink":
     		command => "/bin/ln -s /etc/nginx/sites-available/wp.example.net /etc/nginx/sites-enabled/wp.example.net",
-    		onlyif => "/bin/ln -s /etc/nginx/sites-available/wp.example.net /etc/nginx/sites-enabled/wp.example.net",
+    		onlyif => [ 
+                	"/bin/ln -s /etc/nginx/sites-available/wp.example.net /etc/nginx/sites-enabled/wp.example.net",
+                	"/bin/cat /etc/nginx/sites-enabled/wp.example.net"
+        	]
     		require => File["/etc/nginx/sites-available/wp.example.net"],
     }
     exec { "reload_nginx":
