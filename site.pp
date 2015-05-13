2b040ea1764079c4fb0d84ef8c9d6b452dc33056
quikslvr <yu.elistratov@gmail.com>
2015-05-13 21:58:28 +0300

    Update site.pp

--- site.pp
+++ site.pp
@@ -1,15 +1,18 @@
 
 class nginx {
-    package { "nginx":
+    package { 
+    	"nginx":
 	ensure => latest,
     }
-    service { "nginx":
+    service { 
+    	"nginx":
 	ensure => running,
 	require => Package["nginx"],
 	restart => "/etc/init.d/nginx reload",
     }
  
-    file { "/etc/nginx/sites-available/default":
+    file { 
+    	"/etc/nginx/sites-available/default":
 	source => "puppet://puppet.server/files/cfg/wp.example.net",
 	mode => 644,
 	require => Package["nginx"],
@@ -23,12 +26,14 @@ class nginx {
 	group => "www-data",
 	mode => "0775",
     }
-
- package { 'git':
-        ensure => installed,
+    
+    package { 
+ 	'git':
+ 	ensure => installed,
     }
     
-    vcsrepo { "/var/www/wp/":
+    vcsrepo { 
+    	"/var/www/wp/":
         ensure   => latest,
         owner    => www-data,
         group    => www-data,
@@ -37,26 +42,29 @@ class nginx {
         source   => "https://bitbucket.org/quikslvr/wp_files.git",
         revision => 'master',
     } 
-    
 }
 
 class php-fpm {
-    package { "php5-fpm":
+    package { 
+    	"php5-fpm":
 	ensure => latest,
     }
-    package { "php5-curl":
+    package { 
+    	"php5-curl":
 	ensure => latest,
     }
-    package { "php5-mysql":
+    package { 
+    	"php5-mysql":
 	ensure => latest,
     }
-
-    service { "php5-fpm":
+    service { 
+    	"php5-fpm":
     	ensure => running,
     	require => Package["php5-fpm"],
     	restart => "/etc/init.d/php5-fpm reload",
     }
-    file { "/etc/php5/fpm/pool.d/www.conf":
+    file { 
+    	"/etc/php5/fpm/pool.d/www.conf":
 	source => "puppet://puppet.server/files/cfg/php-fpm.conf",
 	mode => 644,
 	require => Package["php5-fpm"],
@@ -65,14 +73,17 @@ class php-fpm {
 }
 
 class mysql {
-    package { "mysql-server":
+    package { 
+    	"mysql-server":
 	ensure => latest,
     }
-    package { "mysql-client":
-	ensure => latest,
+    package { 
+    	"mysql-client":
+    	ensure => latest,
     }
 
-    service { "mysql":
+    service { 
+    	"mysql":
 	ensure => running,
 	require => Package["mysql-server"],
     }
